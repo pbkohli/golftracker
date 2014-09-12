b550ff71e4f6e5cae1d3ec266c4b1e4dcabe7568
Peter Kohli <pkohli@g2systemsllc.com>
2014-09-12 15:04:52 -0400

    Creating User class

--- golftracker/conf/evolutions/default/1.sql
+++ golftracker/conf/evolutions/default/1.sql
@@ -9,7 +9,16 @@ create table course (
   constraint pk_course primary key (id))
 ;
 
+create table user (
+  email                     varchar(255) not null,
+  name                      varchar(255),
+  password                  varchar(255),
+  constraint pk_user primary key (email))
+;
+
 create sequence course_seq;
+
+create sequence user_seq;
 
 
 
@@ -20,7 +29,11 @@ SET REFERENTIAL_INTEGRITY FALSE;
 
 drop table if exists course;
 
+drop table if exists user;
+
 SET REFERENTIAL_INTEGRITY TRUE;
 
 drop sequence if exists course_seq;
+
+drop sequence if exists user_seq;
 
